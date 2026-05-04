faeeaf7f2eb1c198b57d435578906b75d7869b60
erwinnp <moongraph21@gmail.com>
2026-05-04 23:27:30 +0700

    nvwin configutaion

---
+++ plugin/snacks.lua
@@ -0,0 +1,22 @@
+vim.api.nvim_create_autocmd('BufReadPre', {
+  once = true,
+  callback = function()
+    vim.pack.add({ 'https://github.com/folke/snacks.nvim' })
+
+    local snacks = require('snacks')
+    snacks.setup()
+
+    local function map(mode, key, cmd, desc)
+      vim.keymap.set(
+        mode,
+        key,
+        cmd,
+        { desc = desc, noremap = true, silent = true }
+      )
+    end
+
+    map('n', '<leader>lg', function()
+      snacks.lazygit()
+    end, 'Lazygit')
+  end,
+})
